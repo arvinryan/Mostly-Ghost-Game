6797f81455c186aa2fa82bb2102dad107735ca00
arvinryan <arvinryanhf17@gmail.com>
2025-02-16 14:57:13 +0800

    commit

--- Assets/Ghost Character/PlayerMovement.cs
+++ Assets/Ghost Character/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class scriptnew : MonoBehaviour
 {
     public float speed;
+    public float rotationSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,6 +20,13 @@ public class scriptnew : MonoBehaviour
         Vector3 movementDirection = new Vector3 (horizontalInput, 0, verticalInput);
         movementDirection.Normalize();
 
-        transform.Translate(movementDirection * speed * Time.deltaTime);
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime );        
+        }
     }
 }
