e2ef93ac1ead79a5f1b42356e781ad7e16f6fefb
arvinryan <arvinryanhf17@gmail.com>
2025-03-29 18:09:53 +0800

    added

---
+++ Assets/FollowScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System;
+
+public class FollowScript : MonoBehaviour
+{
+    public Transform targetObj;
+    private Transform self;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        self = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        self.transform.position = Vector3.MoveTowards (self.position, targetObj.position, 5* Time.deltaTime);
+    }
+}
---
+++ Assets/WinLoose.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class WinLoose : MonoBehaviour
+{
+    public void WinLevel()
+    {
+        Debug.Log("You Win!"); 
+    }
+    public void LooseLevel()
+    {
+        Debug.Log("You Loose!");  
+    }
+ 
+}
