41c30b86c227cf937d109b809c7fd9c3049b1569
arvinryan <arvinryanhf17@gmail.com>
2025-04-01 18:40:25 +0800

    added

--- Assets/Scripts/WinTrigger.cs
+++ Assets/Scripts/WinTrigger.cs
@@ -1,7 +1,9 @@
     using UnityEngine;
+    using UnityEngine.SceneManagement;
 
     public class WinTrigger : MonoBehaviour
     {
+        public string sceneToLoadOnLose = "WinScene"; // Name of the scene to load on game over
         void OnTriggerEnter(Collider other)
         {
             // Check if the entering object is the player (using tags, for example)
@@ -17,7 +19,13 @@
             // Implement your win game logic here
             Debug.Log("You got the Blue Orb from the Enemies, Good Job!");
             Debug.Log("You Win!");
+            ReloadScene();
             // Example: Load a new scene
             // UnityEngine.SceneManagement.SceneManager.LoadScene("WinScene");
+
         }
+         void ReloadScene()
+    {
+        SceneManager.LoadScene(sceneToLoadOnLose);
+    }
     }
---
+++ Assets/WinScene.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScene : MonoBehaviour
+{
+    public void WinGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+ 
+}
